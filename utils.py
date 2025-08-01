1e448af94b52e41ce1e39978fb2b675361991d43
Akshara <aksharapvijayan@gmail.com>
2025-08-01 15:38:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,18 @@
-import fitz
 from gtts import gTTS
 from tempfile import NamedTemporaryFile
-from IPython.display import Audio, display
+import fitz  # PyMuPDF
+from bs4 import BeautifulSoup
 
 def parse_pdf(file):
     doc = fitz.open("pdf", file.read())
-    return " ".join(page.get_text() for page in doc)
+    return " ".join([page.get_text() for page in doc])
 
 def parse_html(file):
-    from bs4 import BeautifulSoup
-    return BeautifulSoup(file.read(), "html.parser").get_text()
+    soup = BeautifulSoup(file.read(), 'html.parser')
+    return soup.get_text()
 
 def speak_response(text):
-    tts = gTTS(text=text, lang="en")
-    with NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
-        tts.save(tmp.name)
-        data = open(tmp.name, "rb").read()
-    return data
+    tts = gTTS(text=text, lang='en')
+    with NamedTemporaryFile(delete=False, suffix=".mp3") as fp:
+        tts.save(fp.name)
+        return open(fp.name, 'rb').read()
